a8b2dddd950b2f5439f0272fa5e93a5209d6cb6b
Oliver Layer <o.layer@celonis.de>
2022-01-31 17:05:46 +0100

    basic message (de)serializing, basic server

---
+++ messages.py
@@ -0,0 +1,303 @@
+from __future__ import annotations
+from typing import List, Dict, Tuple, Optional
+from abc import ABC, abstractmethod
+from enum import Enum
+from dataclasses import dataclass
+import json
+
+
+@dataclass
+class ObjectFile:
+    file_name: str
+    size: int
+    content: bytearray
+
+
+class MessageType(Enum):
+    ARGUMENT_MESSAGE = 1
+    DEPENDENCY_REQUEST_MESSAGE = 2
+    DEPENDENCY_REPLY_MESSAGE = 3
+    COMPILATION_RESULT_MESSAGE = 4
+
+
+class Message(ABC):
+    """Abstract class for all messages."""
+
+    MINIMUM_SIZE_BYTES = 9
+    """Minimum size of a message in bytes."""
+    TYPE_OFFSET = 0
+    """Offset of the message type field."""
+    JSON_SIZE_OFFSET = 1
+    """Offset of the JSON size field."""
+    JSON_OFFSET = 9
+    """Offset of the JSON field."""
+
+    def __init__(self, message_type: MessageType) -> None:
+        """Create a new message of given type."""
+        self.message_type = message_type
+
+    @abstractmethod
+    def get_json_str(self) -> str:
+        """To be implemented by classes that inherit. Defines how the JSON looks like."""
+        ...
+
+    def get_further_payload_size(self) -> int:
+        """To be overwritten by subclasses to define how much payload is followed after the JSON object."""
+        return 0
+
+    def set_further_payload(self, further_payload: bytearray):
+        """To be overwritten by subclasses that have extra payload."""
+        pass
+
+    def get_further_payload(self) -> bytearray:
+        """To be overwritten by subclasses to append something after the message's JSON object."""
+        return bytearray()
+
+    def to_bytes(self) -> bytearray:
+        """Serializes the message as a bytearray."""
+        json_bytes: bytearray = bytearray(self.get_json_str(), "utf-8")
+
+        message_type_bytes: bytearray = bytearray(
+            self.message_type.value.to_bytes(length=1, byteorder="little", signed=False)
+        )
+
+        json_size: int = len(json_bytes)
+        json_size_bytes: bytearray = bytearray(
+            json_size.to_bytes(length=8, byteorder="little")
+        )
+
+        payload: bytearray = (
+            message_type_bytes
+            + json_size_bytes
+            + json_bytes
+            + self.get_further_payload()
+        )
+        return payload
+
+    @staticmethod
+    def from_bytes(bytes: bytearray) -> Tuple[int, Optional[Message]]:
+        """Deserializes the message from a bytearray.
+        Returns a tuple where the first element indicates whether or not more bytes
+        are needed to construct the message.
+        - A positive value means that this amount of bytes is needed to parse the message.
+        - A negative value means that the byte buffer supplied is too large and there are more
+        messages contained in it.
+        - 0 means that the amount of supplied bytes was exactly fitting the message.
+
+        The second element of the tuple is the message."""
+        len_bytes = len(bytes)
+        if len_bytes < Message.MINIMUM_SIZE_BYTES:
+            # we need at least the message_type and json_size field to further parse
+            return (Message.MINIMUM_SIZE_BYTES - len_bytes, None)
+
+        message_type: MessageType = MessageType(int(bytes[Message.TYPE_OFFSET]))
+        json_size: int = int.from_bytes(
+            bytes[Message.JSON_SIZE_OFFSET : Message.JSON_OFFSET],
+            byteorder="little",
+            signed=False,
+        )
+
+        size_difference: int = Message.MINIMUM_SIZE_BYTES + json_size - len_bytes
+        if size_difference > 0:
+            # we need more data to parse the message
+            return (size_difference, None)
+
+        json_dict: dict = json.loads(
+            bytes[Message.JSON_OFFSET : Message.JSON_OFFSET + json_size].decode(
+                encoding="utf-8"
+            )
+        )
+
+        message: Message
+        if message_type == MessageType.ARGUMENT_MESSAGE:
+            message = ArgumentMessage.from_dict(json_dict)
+        elif message_type == MessageType.DEPENDENCY_REQUEST_MESSAGE:
+            message = DependencyRequestMessage.from_dict(json_dict)
+        elif message_type == MessageType.DEPENDENCY_REPLY_MESSAGE:
+            message = DependencyReplyMessage.from_dict(json_dict)
+        elif message_type == MessageType.COMPILATION_RESULT_MESSAGE:
+            message = CompilationResultMessage.from_dict(json_dict)
+        else:
+            raise ValueError(f"{message_type} is not a valid message type.")
+
+        further_payload_size: int = message.get_further_payload_size()
+        if further_payload_size == 0:
+            # no further payload, return the message
+            return (size_difference, message)
+        else:
+            size_difference += further_payload_size
+            if size_difference > 0:
+                # further payload and we do not have enough buffer
+                return (size_difference, None)
+            else:
+                message.set_further_payload(
+                    bytes[Message.MINIMUM_SIZE_BYTES + json_size : len_bytes]
+                )
+                return (size_difference, message)
+
+
+class ArgumentMessage(Message):
+    """Initial message in the protocol. Client sends arguments, working directory and
+    dependencies (file paths and their SHA1SUM)."""
+
+    def __init__(
+        self, arguments: List[str], cwd: str, dependencies: Dict[str, str]
+    ) -> None:
+        self.arguments = arguments
+        self.cwd = cwd
+        self.dependencies = dependencies
+
+        super().__init__(MessageType.ARGUMENT_MESSAGE)
+
+    def get_json_str(self) -> str:
+        return json.dumps(
+            {
+                "arguments": self.arguments,
+                "cwd": self.cwd,
+                "dependencies": self.dependencies,
+            }
+        )
+
+    def get_arguments(self) -> List[str]:
+        """Returns the arguments as a list of strings."""
+        return self.arguments
+
+    def get_cwd(self) -> str:
+        """Returns the current working directory."""
+        return self.cwd
+
+    def get_dependencies(self) -> Dict[str, str]:
+        """Returns a dictionary with dependencies."""
+        return self.dependencies
+
+    @staticmethod
+    def from_dict(json_dict: dict) -> ArgumentMessage:
+        return ArgumentMessage(
+            json_dict["arguments"], json_dict["cwd"], json_dict["dependencies"]
+        )
+
+
+class DependencyRequestMessage(Message):
+    """Message that lets the server request exactly one dependency from the client."""
+
+    def __init__(self, dependency_hash: str) -> None:
+        self.dependency_hash = dependency_hash
+
+        super().__init__(MessageType.DEPENDENCY_REQUEST_MESSAGE)
+
+    def get_json_str(self) -> str:
+        return json.dumps({"sha1sum": self.dependency_hash})
+
+    def get_hash(self) -> str:
+        """Returns the SHA1SUM of the dependency."""
+        return self.dependency_hash
+
+    @staticmethod
+    def from_dict(json_dict: dict) -> DependencyRequestMessage:
+        return DependencyRequestMessage(json_dict["dependencies"])
+
+
+class DependencyReplyMessage(Message):
+    """Message that contains exactly one previously requested file."""
+
+    def __init__(
+        self, dependency_hash: str, dependency_size: int, dependency_content: bytearray
+    ) -> None:
+        self.dependency_hash = dependency_hash
+        self.dependency_size = dependency_size
+        self.dependency_content = dependency_content
+
+        super().__init__(MessageType.DEPENDENCY_REPLY_MESSAGE)
+
+    def get_json_str(self) -> str:
+        return json.dumps(
+            {"sha1sum": self.dependency_hash, "size": self.dependency_size}
+        )
+
+    def get_content(self) -> bytearray:
+        return self.dependency_content
+
+    def get_hash(self) -> str:
+        """Returns the hash of the dependency."""
+        return self.dependency_hash
+
+    def get_size(self) -> int:
+        """Returns the size of the dependency."""
+        return self.dependency_size
+
+    def get_further_payload(self) -> bytearray:
+        """Overwritten so that the dependency's content is appended to the message."""
+        return self.dependency_content
+
+    def set_further_payload(self, further_payload: bytearray):
+        """Overwritten so that the dependencies' content can be set."""
+        self.dependency_content = further_payload
+
+    def get_further_payload_size(self) -> int:
+        """Overwritten so that the dependency's payload size can be calculated."""
+        return self.dependency_size
+
+    @staticmethod
+    def from_dict(json_dict: dict) -> DependencyReplyMessage:
+        return DependencyReplyMessage(
+            json_dict["sha1sum"], json_dict["size"], bytearray()
+        )
+
+
+class CompilationResultMessage(Message):
+    """Message that contains the compilation result (list of files).
+    A file contains the filename (relative to the working directory) and
+    the size of the file in bytes."""
+
+    def __init__(self, object_files: List[ObjectFile]) -> None:
+        self.object_files = object_files
+
+        super().__init__(MessageType.COMPILATION_RESULT_MESSAGE)
+
+    def get_json_str(self) -> str:
+        files = []
+        for object_file in self.object_files:
+            files.append({"filename": object_file.file_name, "size": object_file.size})
+
+        return json.dumps({"files": files})
+
+    def get_object_files(self) -> List[ObjectFile]:
+        return self.object_files
+
+    def get_further_payload(self) -> bytearray:
+        """Overwritten so that the dependencies' content can be appended to the message."""
+        further_payload = bytearray()
+
+        for file in self.object_files:
+            further_payload += file.content
+
+        return further_payload
+
+    def set_further_payload(self, further_payload: bytearray):
+        """Overwritten so that the dependencies' content can be set."""
+        current_payload_offset: int = 0
+        for file in self.object_files:
+            file.content = further_payload[
+                current_payload_offset : current_payload_offset + file.size
+            ]
+            current_payload_offset += file.size
+
+    def get_further_payload_size(self) -> int:
+        """Overwritten so that the dependencies' payload size can be calculated."""
+        total_size: int = 0
+
+        for object_file in self.object_files:
+            total_size += object_file.size
+
+        return total_size
+
+    @staticmethod
+    def from_dict(json_dict: dict) -> CompilationResultMessage:
+        object_files: List[ObjectFile] = []
+        for file in json_dict[0]:
+            object_file_size = file["size"]
+
+            object_file = ObjectFile(file["filename"], object_file_size, bytearray())
+            object_files.append(object_file)
+
+        return CompilationResultMessage(object_files)
