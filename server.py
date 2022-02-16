e4107d2303c6f45664c95d93cc1740261273b88b
Oliver Layer <o.layer@celonis.de>
2022-02-16 14:31:56 +0100

    added logging

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
+import logging
+
 from homcc.server.server import *
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
     port: int = 3633
     server: TCPServer = start_server(port=port)
 
     with server:
-        input("Press key to exit")
+        input("Press key to exit\n")
         stop_server(server)
