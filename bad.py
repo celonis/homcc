85c1645503d548f349fa165aaa6ea0789a9932c9
Simon Pirsch <s.pirsch@celonis.com>
2022-03-08 18:07:26 +0100

    CPL-6289: add test coverage and temporary file for testing

---
+++ bad.py
@@ -0,0 +1,4 @@
+def BadFunctionWithMistakes():
+    print("bad function");
+    pass # Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    return
