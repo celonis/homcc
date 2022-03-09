0195405cf6a46666089592ace3615cd83b035efb
Simon Pirsch <s.pirsch@celonis.com>
2022-03-09 10:54:40 +0100

    CPL-6289: changed lint config to google def

--- bad.py
+++ bad.py
@@ -1,4 +1,5 @@
-def BadFunctionWithMistakes():
+# pylint: disable-next=unbalanced-tuple-unpacking
+def BadFunctionWithMistakes() -> int:
     print("bad function");
     pass # Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
     return
