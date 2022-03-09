621c8b6f1d239000546dbde1a09f0090477255b0
Simon Pirsch <s.pirsch@celonis.com>
2022-03-09 16:19:19 +0100

    CPL-6289: removed bad file for testing purposes

--- bad.py
+++
@@ -1,5 +0,0 @@
-# pylint: disable-next=unbalanced-tuple-unpacking
-def BadFunctionWithMistakes() -> int:
-    print("bad function");
-    pass # Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    return
