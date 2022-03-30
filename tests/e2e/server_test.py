b728c1ef60480451bf119efdba240362649927eb
Oliver Layer <o.layer@celonis.de>
2022-03-30 15:03:47 +0200

    kill test server process

--- tests/e2e/server_test.py
+++ tests/e2e/server_test.py
@@ -30,14 +30,18 @@ class TestEndToEnd:
         yield
         Path("e2e-test").unlink(missing_ok=True)
 
-    @pytest.mark.timeout(60)
+    @pytest.mark.timeout(10)
     def test_end_to_end(self):
-        with self.start_server():
+
+        with self.start_server() as server_process:
             client_stdout = self.start_client()
 
             # make sure we actually compiled at the server (and did not fall back to local compilation),
             # i.e. look at the log messages if the compilation of the file on the server side was okay
             assert '"return_code": 0' in client_stdout
+            assert "Compiling locally instead" not in client_stdout
 
             binary_stdout = subprocess.check_output(["./e2e-test"]).decode("utf-8")
             assert binary_stdout == "homcc\n"
+
+            server_process.kill()
