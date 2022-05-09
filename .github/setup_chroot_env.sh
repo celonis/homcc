790a065acf2e4f4b50d12483ac6368b576fb40d1
Simon Pirsch <s.pirsch@celonis.com>
2022-05-09 10:56:19 +0200

    CPL-6546: workflow debug#12

--- .github/setup_chroot_env.sh
+++ .github/setup_chroot_env.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
 FILE="/etc/schroot/chroot.d/$1.conf"
-GROUPS=`id -nG | sed 's/ /,/g'`
 
 cat > $FILE << EOF
 [$1]
 description=Test Environment $1
 directory=/var/chroot/$1
-root-groups=$GROUPS
-groups=$GROUPS
+root-groups=$2
 type=directory
 EOF
