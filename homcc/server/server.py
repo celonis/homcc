1a62e97dd59dd98543e6c3bd5ae1270fa9f98f5d
Oliver Layer <o.layer@celonis.de>
2022-05-11 12:56:14 +0200

    ignore empty config lines

--- homcc/server/server.py
+++ homcc/server/server.py
@@ -168,8 +168,10 @@ class TCPRequestHandler(socketserver.BaseRequestHandler):
                 self.compiler_arguments.compiler,
             )
             self.close_connection(
-                f"""Compiler '{self.compiler_arguments.compiler}' is not available on the server,
-                can not compile remotely""",
+                (
+                    f"""Compiler '{self.compiler_arguments.compiler}' is not available on the server,
+                    can not compile remotely"""
+                ),
             )
             return
 
