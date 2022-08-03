a3ce78b2d75693821ce13f456f6c04612b64c683
arin-mirza <a.mirza@celonis.com>
2022-08-03 14:07:02 +0200

    Create directory for report (csv) if it does not exist already

--- object_file_compression_analysis/main.py
+++ object_file_compression_analysis/main.py
@@ -50,7 +50,10 @@ def main():
         print(f"Benchmarking method {algorithm.name} with compression level {compression_level}")
         result = algorithm.apply()
 
-        with open(f"benchmarks/{compiler}/benchmark_{compiler}_{algorithm.name}_{mode}_{compression_level}.csv", 'w') as report:
+        report_name = f"benchmarks/{compiler}/benchmark_{compiler}_{algorithm.name}_{mode}_{compression_level}.csv"
+        os.makedirs(os.path.dirname(report_name), exist_ok=True)
+
+        with open(report_name, 'w') as report:
             header = "Filename, Size_Original, Size_Compressed, Size_Ratio, Time_Compress, Time_Decompress"
             report.write(header + '\n')
             for file, stats in result.items():
