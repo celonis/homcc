6985065c19e5b82cfca919c438c8af0687818008
yagmurdum <rainduman@gmail.com>
2022-10-27 15:09:54 +0200

    edited according to pr reviews

--- homcc/monitor/main.py
+++ homcc/monitor/main.py
@@ -12,7 +12,7 @@ def main():
     app: QApplication = QApplication()
     table: Table = Table(Table.column_headers, Table.data)
     table.table.setWindowTitle("homcc Monitor")
-    table.table.resize(500, 200.)
+    table.table.resize(500, 200)
     table.render()
     table.table.show()
     sys.exit(app.exec_())
