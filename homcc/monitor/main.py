3a3fb446abeec0b5ff9d82123e8eeb150116ad76
Yagmur Duman <112613067+yagmurdum@users.noreply.github.com>
2022-11-04 13:13:18 +0100

    Cpl 7606 monitor create basic pyside prototype (#60)
    * updated client path for compilation stdout and stderr
    
    * updated client path for compilation stdout and stderr
    
    * monitor prototype
    
    * format changes
    
    * format changes
    
    * format changes
    
    * edited according to pr reviews
    
    * edited according to pr reviews
    
    * edited according to pr reviews
    
    * edited according to pr reviews
    
    * linter and format check
    
    * linter and format check
    
    * format change

---
+++ homcc/monitor/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+homcc monitor
+"""
+import sys
+
+from PySide2.QtWidgets import QApplication  # type: ignore
+from table import Table
+
+data_info = [
+    ("foo", "Sending", "foo.cpp", "13sec"),
+    ("bar", "Compiling", "bar.cpp", "4sec"),
+]
+
+
+def main():
+
+    app: QApplication = QApplication()
+    table: Table = Table(data_info)
+    # table.table.setWindowTitle("homcc Monitor")
+    table.table.resize(500, 200)
+    table.render()
+    table.table.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
